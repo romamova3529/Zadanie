d18577fe42684372d77d17d222f9088ec30d5adc
romamova3529 <svetlanaromanova0199@gmail.com>
2022-12-20 16:12:00 +0700

    commit

--- 6/задание 2.js
+++
@@ -1,9 +0,0 @@
-const array = [];
-for (let i = 1; i < 29; i++){
-array.push(i);
-}
-array.forEach(e => {
-const date = new Date(2023, 1, e);
-const day = new Intl.DateTimeFormat('ru-RU', { weekday: 'long'}).format(date);
-console.log(`${e} Февраля, ${day}`);
-});
